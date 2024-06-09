7d140718eed91efa2de42bd1a5995210752451d6
gangmin2 <min0115b@gmail.com>
2024-06-09 16:01:03 +0900

    Feat: Content 영역 구현

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,11 +1,13 @@
 import NavBar from "./NavBar";
 import Banner from "./Banner";
+import Content from "./Content";
 
 const LandingPage: React.FC = () => {
   return (
     <>
       <NavBar />
       <Banner />
+      <Content />
     </>
   )
 }

b43e6d7a3f3167844573ff609cabd8bddf3d8822
gangmin2 <min0115b@gmail.com>
2024-06-09 17:28:37 +0900

    Feat: input 요소, 포커스 시 border color 변경

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -15,8 +15,8 @@ const LoginPage: React.FC = () => {
         <Flex flexDir='column' bg='white' p='24px' borderRadius='16px' align='center' maxW='500px' w='100%' >
           <Image src={logo} boxSize='150px' objectFit='contain' mb='20px' />
           <FormControl>
-            <Input type="text" placeholder='아이디' mb='12px' />
-            <Input type="password" placeholder='비밀번호' mb='16px' />
+            <Input type="text" placeholder='아이디' mb='12px' focusBorderColor='green.400' />
+            <Input type="password" placeholder='비밀번호' mb='16px' focusBorderColor='green.400' />
             <Button type="submit" w='100%' bg='green.400' color='white' _hover={{ bg: 'green.500' }} variant='solid' mb='16px'>로그인</Button>
           </FormControl>
           <Flex justify='space-between' w='100%' >

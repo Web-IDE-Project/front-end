eec9c02877b3322cb757c4a94bfd8e5807ebc760
yubin <cyoobin@naver.com>
2024-06-10 15:22:06 +0900

    Design: 에디터 페이지 코드 편집기 UI 구현

--- src/pages/EditorPage/EditorPage.tsx
+++ src/pages/EditorPage/EditorPage.tsx
@@ -1,10 +1,26 @@
-import { Box, Flex } from '@chakra-ui/react'
+import {
+  Box,
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  Flex,
+} from '@chakra-ui/react'
 import NavBar from './NavBar'
 import MenuBar from './MenuBar'
 import Explorer from './Explorer'
 import PermissionSettings from './PermissionSettings'
+import { useState } from 'react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import { UnControlled as CodeMirror } from 'react-codemirror2'
+import 'codemirror/lib/codemirror.css'
+import 'codemirror/theme/base16-light.css'
+import 'codemirror/mode/javascript/javascript'
+import 'codemirror/mode/python/python'
+import 'codemirror/mode/clike/clike'
 
 const EditorPage = () => {
+  const [value, setValue] = useState('console.log(hello);')
+
   return (
     <>
       {/* SECTION 상단바 - 로고, 저장/실행 버튼 */}
@@ -21,11 +37,47 @@ const EditorPage = () => {
           <PermissionSettings />
         </Box>
 
-        {/* SECTION 에디터 영역 */}
-        <Box></Box>
+        {/* SECTION 에디터/터미널 영역 */}
+        <Flex direction="column" w="100%">
+          {/* SECTION 에디터 영역 */}
+          <Box p={2} fontSize="sm">
+            <Breadcrumb
+              spacing="8px"
+              separator={<ChevronRightIcon color="gray.500" />}
+            >
+              <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink href="#">Home</BreadcrumbLink>
+              </BreadcrumbItem>
 
-        {/* SECTION 터미널 영역 */}
-        <Box></Box>
+              <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink href="#">About</BreadcrumbLink>
+              </BreadcrumbItem>
+
+              <BreadcrumbItem isCurrentPage>
+                <BreadcrumbLink href="#">Contact</BreadcrumbLink>
+              </BreadcrumbItem>
+            </Breadcrumb>
+          </Box>
+
+          <Box flexGrow={1}>
+            <CodeMirror
+              value={value}
+              options={{
+                mode: 'javascript', // 언어 모드 설정(text/x-java(java), python, javascript, text/x-csrc(c), text/x-c++src(c++))
+                theme: 'base16-light', // 편집기 테마 설정
+                lineNumbers: true,
+              }}
+              onChange={(editor, data, value) => {
+                setValue(value)
+              }}
+              autoCursor
+              autoScroll
+            />
+          </Box>
+
+          {/* SECTION 터미널 영역 */}
+          <Box></Box>
+        </Flex>
       </Flex>
     </>
   )

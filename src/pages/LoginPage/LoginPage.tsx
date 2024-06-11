d3233c4ef078576ab6fe3b66e4fcc0c8f7baa448
gangmin2 <min0115b@gmail.com>
2024-06-12 05:41:51 +0900

    feat: 로그인 기능 구현

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,19 +1,64 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Button, Flex, FormControl, Image, Input, Text, Box, Divider, AbsoluteCenter } from '@chakra-ui/react';
 import logo from '../../assets/images/logo.png';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import API from '@/services/API';
+
+interface LoginFormFields {
+  username: string;
+  password: string;
+}
 
 const LoginPage: React.FC = () => {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormFields>();
+
+  const onSubmit: SubmitHandler<LoginFormFields> = async (data) => {
+    try {
+      const response = await API.post('/api/auth/login', data);
+      console.log(response);
+      navigate('/container/my');
+    } catch (error: unknown) {
+      if (typeof error === 'object' && error !== null && 'message' in error) {
+        alert((error as { message: string }).message);
+        console.error('로그인 실패:', error);
+      } else {
+        alert('알 수 없는 오류가 발생했습니다.');
+        console.error('로그인 실패:', error);
+      }
+    }
+  };
+
   return (
     <>
-      <Flex w='full' h='100vh' bg='gray.50' align='center' justify='center'>
+      <Flex w='100%' h='100vh' bg='gray.50' align='center' justify='center'>
         <Flex flexDir='column' bg='white' p={6} borderRadius={10} align='center' maxW='500px' w='full' >
-          <Image src={logo} boxSize='150px' objectFit='contain' mb={5} />
-          <FormControl>
-            <Input type="text" placeholder='아이디' mb={3} focusBorderColor='green.400' />
-            <Input type="password" placeholder='비밀번호' mb={4} focusBorderColor='green.400' />
-            <Button type="submit" w='full' bg='green.400' color='white' _hover={{ bg: 'green.500' }} variant='solid' mb={4}>로그인</Button>
-          </FormControl>
-          <Flex justify='space-between' w='full' >
+          <Image src={logo} boxSize='150px' objectFit='contain' mb='20px' />
+          <form onSubmit={handleSubmit(onSubmit)} style={{ width: '100%', }} >
+            <FormControl>
+              <Input
+                {...register('username', { required: true })}
+                type="text"
+                placeholder='아이디'
+                mb={3}
+                focusBorderColor='green.400'
+              />
+              <Input
+                {...register('password', { required: true })}
+                type="password"
+                placeholder='비밀번호'
+                focusBorderColor='green.400'
+              />
+              {errors.username && <Text color="red">아이디를 입력해주세요.</Text>}
+              {errors.password && <Text color="red">비밀번호를 입력해주세요.</Text>}
+              <Button type="submit" w='full' bg='green.400' color='white' _hover={{ bg: 'green.500' }} variant='solid' my={4}>로그인</Button>
+            </FormControl>
+          </form>
+          <Flex justify='space-between' w='100%' >
             <Link to="/signup">
               <Text fontSize='small'>회원가입 하러가기</Text>
             </Link>

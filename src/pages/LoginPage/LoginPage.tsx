df5b41adfeb78f7141a21ab59e3356fe30d90504
gangmin2 <min0115b@gmail.com>
2024-06-12 05:21:04 +0900

    Feat: 소셜로그인 수정 및 반응형 디자인 수정

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,25 +1,19 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Button, Flex, FormControl, Image, Input, Text, Box, Divider, AbsoluteCenter } from '@chakra-ui/react';
 import logo from '../../assets/images/logo.png';
 
 const LoginPage: React.FC = () => {
-  const navigate = useNavigate();
-
-  const onClickSocialButton = (path: string) => {
-    navigate('/api/oauth2/authorization/' + path);
-  }
-
   return (
     <>
-      <Flex w='100%' h='100vh' bg='gray.50' align='center' justify='center'>
-        <Flex flexDir='column' bg='white' p='24px' borderRadius='16px' align='center' maxW='500px' w='100%' >
-          <Image src={logo} boxSize='150px' objectFit='contain' mb='20px' />
+      <Flex w='full' h='100vh' bg='gray.50' align='center' justify='center'>
+        <Flex flexDir='column' bg='white' p={6} borderRadius={10} align='center' maxW='500px' w='full' >
+          <Image src={logo} boxSize='150px' objectFit='contain' mb={5} />
           <FormControl>
-            <Input type="text" placeholder='아이디' mb='12px' focusBorderColor='green.400' />
-            <Input type="password" placeholder='비밀번호' mb='16px' focusBorderColor='green.400' />
-            <Button type="submit" w='100%' bg='green.400' color='white' _hover={{ bg: 'green.500' }} variant='solid' mb='16px'>로그인</Button>
+            <Input type="text" placeholder='아이디' mb={3} focusBorderColor='green.400' />
+            <Input type="password" placeholder='비밀번호' mb={4} focusBorderColor='green.400' />
+            <Button type="submit" w='full' bg='green.400' color='white' _hover={{ bg: 'green.500' }} variant='solid' mb={4}>로그인</Button>
           </FormControl>
-          <Flex justify='space-between' w='100%' >
+          <Flex justify='space-between' w='full' >
             <Link to="/signup">
               <Text fontSize='small'>회원가입 하러가기</Text>
             </Link>
@@ -27,16 +21,22 @@ const LoginPage: React.FC = () => {
               <Text fontSize='small'>아이디 비밀번호 찾기</Text>
             </Link>
           </Flex>
-          <Box pos='relative' py='12' width='100%'>
+          <Box pos='relative' py={12} width='100%'>
             <Divider />
-            <AbsoluteCenter bg='white' w='50px'>
+            <AbsoluteCenter bg='white' w='fit-content' p={4}>
               <Text color='gray.400' textAlign='center' fontSize='small'>또는</Text>
             </AbsoluteCenter>
           </Box>
-          <Flex flexDir='column' w='100%' gap='10px' >
-            <Button onClick={() => onClickSocialButton('kakao')} bg='yellow.300' _hover={{ bg: 'yellow.400' }}>카카오계정으로 로그인</Button>
-            <Button onClick={() => onClickSocialButton('naver')} bg='green.400' color='white' _hover={{ bg: 'green.500' }}>네이버로 로그인</Button>
-            <Button onClick={() => onClickSocialButton('google')} colorScheme='gray'>구글로 로그인</Button>
+          <Flex flexDir='column' w='full' gap={3} >
+            <Link to='/oauth2/authorization/kakao' >
+              <Button bg='yellow.300' _hover={{ bg: 'yellow.400' }} w='full'>카카오계정으로 로그인</Button>
+            </Link>
+            <Link to='/oauth2/authorization/naver'>
+              <Button bg='green.400' color='white' _hover={{ bg: 'green.500' }} w='full'>네이버로 로그인</Button>
+            </Link>
+            <Link to='/oauth2/authorization/google'>
+              <Button colorScheme='gray' w='full'>구글로 로그인</Button>
+            </Link>
           </Flex>
         </Flex>
       </Flex>

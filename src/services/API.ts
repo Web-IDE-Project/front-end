dbab1bce943bc7b5000c1cb9b2b3958b5c7cb679
yubin <cyoobin@naver.com>
2024-06-21 03:40:30 +0900

    Fix: axios baseURL 하드코딩

--- src/services/API.ts
+++ src/services/API.ts
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_SERVER_BASE_URL,
+  // baseURL: import.meta.env.VITE_SERVER_BASE_URL,
+  baseURL: 'http://localhost:8080',
   withCredentials: true,
 })
 

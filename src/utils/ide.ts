465881955fd69295f2b45b5d8a20994efe5ba11c
yubin <cyoobin@naver.com>
2024-06-21 16:35:46 +0900

    Remove: 불필요한 import 제거

--- src/utils/ide.ts
+++ src/utils/ide.ts
@@ -1,5 +1,3 @@
-import { Entry } from '@/models/entry'
-
 export function isEditable(status: string, category: string, isOwner: boolean) {
   // 상태가 종료/해결일 경우
   if (status === 'SOLVE' || status === 'COMPLETE') {

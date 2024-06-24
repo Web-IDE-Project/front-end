0769488c9dd9eb9228f8856f1ef45c7c014aa6a9
yubin <cyoobin@naver.com>
2024-06-24 23:53:41 +0900

    Fix: server proxy 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,23 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+import { env } from 'process'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
+    proxy: {
+      '/api': {
+        target: env.VITE_SERVER_BASE_URL,
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, ''),
+        ws: true,
+      },
+    },
   },
   resolve: {
-    alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
-});
+})
